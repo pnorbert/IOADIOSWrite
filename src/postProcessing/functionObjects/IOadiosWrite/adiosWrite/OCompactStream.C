149924203e87e4c2f21ddd1478957984c55307e2
mark <mark@opencfd>
2016-04-15 19:46:00 +0200

    ENH: provide various streaming classes
    - counter, external buffer

---
+++ src/postProcessing/functionObjects/IOadiosWrite/adiosWrite/OCompactStream.C
@@ -0,0 +1,89 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2016 OpenCFD Ltd
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "OCompactStream.H"
+#include "token.H"
+
+// * * * * * * * * * * * * * * * * Constructor * * * * * * * * * * * * * * * //
+
+Foam::OCompactStream::OCompactStream
+(
+    ostream& os,
+    const string& name,
+    streamFormat format,
+    versionNumber version,
+    compressionType compression
+)
+:
+    OSstream(os, name, format, version, compression)
+{}
+
+
+// * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * * //
+
+Foam::Ostream& Foam::OCompactStream::writeKeyword(const keyType& kw)
+{
+    write(kw);
+    write(char(token::SPACE));
+
+    return *this;
+}
+
+
+Foam::Ostream& Foam::OCompactStream::beginBlock(const word& keyword)
+{
+    write(keyword);
+    beginBlock();
+
+    return *this;
+}
+
+
+Foam::Ostream& Foam::OCompactStream::beginBlock()
+{
+    write(char(token::BEGIN_BLOCK));
+    return *this;
+}
+
+
+Foam::Ostream& Foam::OCompactStream::endBlock()
+{
+    write(char(token::END_BLOCK));
+    return *this;
+}
+
+
+void Foam::OCompactStream::indent()
+{}
+
+
+void Foam::OCompactStream::endl()
+{
+    write('\n');
+    flush();
+}
+
+
+// ************************************************************************* //
